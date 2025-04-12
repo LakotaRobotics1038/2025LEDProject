339c9f15d7d468311fe4125f6ce73128994021c4
Murat65536 <bmuratb502@gmail.com>
2025-04-12 10:51:33 -0400

    Fix syntax issue.

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ controller.configure(
         9: (
             (45, {
                 "D": lambda pin: controller.chasing(pin, (0, 0, 200), (200, 0, 200), 10, 0.01, 20, 50),
-                "E": lambda pin: controller.color_fade(pin, ((255, 0, 0), (0, 255, 0), (0, 0, 255)), 128, 0.01, 0),
+                "E": lambda pin: controller.color_fade(pin, ((255, 0, 0), (0, 255, 0), (0, 0, 255)), 128, 0.01),
                 "X": lambda pin: controller.chasing(pin, (0, 0, 200), (200, 0, 200), 100, 0.1, 10, 10),
                 "G": lambda pin: controller.static_color(pin, (0, 255, 0), 1, True, "D"),
             }),
@@ -133,7 +133,7 @@ controller.configure(
         10: (
             (45, {
                 "D": lambda pin: controller.chasing(pin, (0, 0, 200), (200, 0, 200), 10, 0.01, 20, 50),
-                "E": lambda pin: controller.color_fade(pin, ((255, 0, 0), (0, 255, 0), (0, 0, 255)), 128, 0.01, 0),
+                "E": lambda pin: controller.color_fade(pin, ((255, 0, 0), (0, 255, 0), (0, 0, 255)), 128, 0.01),
                 "X": lambda pin: controller.chasing(pin, (0, 0, 200), (200, 0, 200), 100, 0.1, 10, 10),
                 "G": lambda pin: controller.static_color(pin, (0, 255, 0), 1, True, "D"),
             }),
